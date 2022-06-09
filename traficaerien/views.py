c925cd86743ac76dfa92be6435eba75cdfe458bc
Elise-Beauvy <94043023+Elise-Beauvy@users.noreply.github.com>
2022-06-09 22:22:51 +0200

    html

--- traficaerien/views.py
+++ traficaerien/views.py
@@ -17,76 +17,82 @@ from django import forms
 
 def main(request):
     liste = avion.objects.all()
-    liste1 = piste.objects.all()
-    liste2 = vol.objects.all()
     return render(request, 'main.html', {'liste': liste})
-    return render(request, 'main.html', {'liste1': liste1})
-    return render(request, 'main.html', {'liste2': liste2})
+
 
 def ajout_avion(request):
-    if request.method == "POST":
-        form = AvionForm(request)
+    submitted = False
+    if request.method == 'POST':
+        form = AvionForm(request.POST)
         if form.is_valid():
-            joueur = form.save()
-            return HttpResponseRedirect("/traficaerien/")
-        else:
-            return render(request,"ajout_avion.html",{"form": form})
-    else :
-        form = AvionForm()
-        return render(request,"ajout_avion.html",{"form" : form})
-
-
-
-def traitement_avion(request):
-    lform = AvionForm(request.POST)
-    if lform.is_valid():
-        avion = lform.save(commit=False)
-        avion.save()
-        return HttpResponseRedirect("/traficaerien/")
+            form.save()
+            return HttpResponseRedirect("/trafic/liste_avion/")
     else:
-        return render(request,"ajout_avion.html",{"form": lform})
+        form = AvionForm
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'ajout_avion.html', {'form':form, 'submitted':submitted})
+
+
+def liste_avion(request):
+    liste = vol.objects.all()
+    liste_avion = avion.objects.all()
+    return render(request, 'liste_avion.html', {'liste_avion': liste_avion,"liste":liste})
+
+
+def delete_avion(request, id):
+    avion_list = avion.objects.get(id=id)
+    avion_list.delete()
+    return HttpResponseRedirect("/trafic/liste_avion/")
 
 def ajout_piste(request):
-    if request.method == "POST":
-        form = PisteForm(request)
+    submitted = False
+    if request.method == 'POST':
+        form =PisteForm(request.POST)
         if form.is_valid():
-            piste = form.save()
-            return HttpResponseRedirect("/traficaerien/")
-        else:
-            return render(request,"ajout_piste.html",{"form": form})
-    else :
-        form = PisteForm()
-        return render(request,"ajout_piste.html",{"form" : form})
-
-def traitement_piste(request):
-    lform = PisteForm(request.POST)
-    if lform.is_valid():
-        piste = lform.save(commit=False)
-        piste.save()
-        return HttpResponseRedirect("/traficaerien/")
+            form.save()
+            return HttpResponseRedirect("/trafic/liste_piste/")
     else:
-        return render(request,"ajout_piste.html",{"form": lform})
+        form = PisteForm
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'ajout_piste.html', {'form':form, 'submitted':submitted})
+
+def liste_piste(request):
+   liste = piste.objects.all()
+   liste_piste= piste.objects.all()
+   return render(request, 'liste_piste.html', {'liste_piste': liste_piste,"liste":liste})
+
+
+def delete_piste(request, id):
+    piste_list = piste.objects.get(id=id)
+    piste_list.delete()
+    return HttpResponseRedirect("/trafic/liste_piste/")
+
+
 
 def ajout_vol(request):
-    if request.method == "POST":
-        form = VolForm(request)
+    submitted = False
+    if request.method == 'POST':
+        form = VolForm(request.POST)
         if form.is_valid():
-            vol = form.save()
-            return HttpResponseRedirect("/traficaerien/")
-        else:
-            return render(request,"ajout_vol.html",{"form": form})
-    else :
-        form = VolForm()
-        return render(request,"ajout_vol.html",{"form" : form})
-
-def traitement_vol(request):
-    lform = VolForm(request.POST)
-    if lform.is_valid():
-        vol = lform.save(commit=False)
-        vol.save()
-        return HttpResponseRedirect("/traficaerien/")
+            form.save()
+            return HttpResponseRedirect("/trafic/liste_vol/")
     else:
-        return render(request,"ajout_vol.html",{"form": lform})
+        form = VolForm
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'ajout_vol.html', {'form':form, 'submitted':submitted})
+
+def liste_vol(request):
+   liste = vol.objects.all()
+   liste_vol= vol.objects.all()
+   return render(request, 'liste_vol.html', {'liste_vol': liste_vol,"liste":liste})
+
+def delete_vol(request, id):
+    vol_list = vol.objects.get(id=id)
+    vol_list.delete()
+    return HttpResponseRedirect("/trafic/liste_vol/")
 
 def ajout_aeroport(request):
     submitted = False
@@ -106,16 +112,56 @@ def liste_aeroport(request):
    liste_aeroport = aeroport.objects.all()
    return render(request, 'liste_aeroport.html', {'liste_aeroport': liste_aeroport,"liste":liste})
 
-def update_aeroport(request, id):
-    aeroport_list = aeroport.objects.get(pk=id)
-    form = AeroportForm(request.POST or None, instance=aeroport_list)
-    if form.is_valid():
-        form.save()
-        return redirect("aeroport")
-    return render(request, 'update_aeroport.html', {'aeroport':aeroport, 'form':form})
-
 def delete_aeroport(request, id):
     aeroport_list = aeroport.objects.get(id=id)
     aeroport_list.delete()
     return HttpResponseRedirect("/trafic/liste_aeroport/")
+
+def ajout_compagnie(request):
+    submitted = False
+    if request.method == 'POST':
+        form = CompagnieForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/trafic/liste_compagnie/")
+    else:
+        form = CompagnieForm
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'ajout_compagnie.html', {'form':form, 'submitted':submitted})
+
+def liste_compagnie(request):
+   liste = compagnie.objects.all()
+   liste_compagnie= compagnie.objects.all()
+   return render(request, 'liste_compagnie.html', {'liste_compagnie': liste_compagnie,"liste":liste})
+
+def delete_compagnie(request, id):
+    compagnie_list = compagnie.objects.get(id=id)
+    compagnie_list.delete()
+    return HttpResponseRedirect("/trafic/liste_compagnie/")
+
+
+def ajout_aviontype(request):
+    submitted = False
+    if request.method == 'POST':
+        form =AvionTypeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/trafic/liste_aviontype/")
+    else:
+        form = AvionTypeForm
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'ajout_aviontype.html', {'form':form, 'submitted':submitted})
+
+def liste_aviontype(request):
+   liste = type_avion.objects.all()
+   liste_aviontype= type_avion.objects.all()
+   return render(request, 'liste_aviontype.html', {'liste_aviontype': liste_aviontype,"liste":liste})
+
+
+def delete_aviontype(request, id):
+    aviontype_list = type_avion.objects.get(id=id)
+    aviontype_list.delete()
+    return HttpResponseRedirect("/trafic/liste_aviontype/")
 
