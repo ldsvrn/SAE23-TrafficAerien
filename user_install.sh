81a883291128737f0210b28c56a8e719c1991b56
ldsvrn <louis2555@orange.fr>
2022-06-15 23:38:31 +0200

    revert changes

--- user_install.sh
+++ user_install.sh
@@ -2,9 +2,10 @@
 
 python3 -m venv /home/toto/django/.venv
 source /home/toto/django/.venv/bin/activate
-pip3 install django django-admin mysqlclient whitenoise
+pip3 install django django-admin mysqlclient
 
 python3 /home/toto/django/manage.py makemigration
 python3 /home/toto/django/manage.py migrate
+python3 /home/toto/django/manage.py collectstatic
 
 python3 /home/toto/django/manage.py runserver
