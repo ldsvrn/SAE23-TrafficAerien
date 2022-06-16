256c00dcd0eecdac4151fe73b07850fcfe929282
ldsvrn <louis2555@orange.fr>
2022-06-16 14:07:22 +0200

    dépendance

--- user_install.sh
+++ user_install.sh
@@ -2,11 +2,11 @@
 
 python3 -m venv /home/toto/django/.venv
 source /home/toto/django/.venv/bin/activate
-pip3 install django django-admin mysqlclient gunicorn crispy-bootstrap5
+pip3 install django django-admin mysqlclient gunicorn crispy-bootstrap5 Pillow
 
 python3 /home/toto/django/manage.py makemigration
 python3 /home/toto/django/manage.py migrate
-python3 /home/toto/django/manage.py collectstatic
+python3 /home/toto/django/manage.py collectstatic"
 
 #python3 /home/toto/django/manage.py runserver
 #/home/toto/django/.venv/bin/gunicorn --bind 0.0.0.0:8000 SAE23.wsgi
